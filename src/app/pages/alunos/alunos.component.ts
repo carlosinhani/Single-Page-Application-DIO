47c059656eea27df86c847a24b8403203b43476e
Carlos.inhani <carlos.inhani@gmail.com>
2021-06-26 10:09:07 -0300

    event

--- src/app/pages/alunos/alunos.component.ts
+++ src/app/pages/alunos/alunos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alunos',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alunos.component.css']
 })
 export class AlunosComponent implements OnInit {
+
+  @Input() valorInicial: number = 10;
 
   constructor() { }
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  preserveWhitespaces: true
 })
 export class HomeComponent implements OnInit {
+
+  valor: number;
+  exibeTabela: boolean = false;
+
+  // listaAlunos: alunos[] = [
+  //   {nome: 'Rodrigo', idade: 38, email: 'carlos.inhani@gmail.com', curso: 'Angular avançado'},
+  //   {nome: 'Priscila', idade: 33, email: 'priscila33@yahoo.com', curso: 'Jornalismo'},
+  //   {nome: 'Mathues', idade: 21, email: 'sera@gmail.com', curso: 'Análise de Sistemas'},
+  //   {nome: 'Gabriel', idade: 23, email: 'ocaramagro@gmail.com', curso: 'Análise de Sistemas'},
+  // ]
 
   constructor() { }
 
   ngOnInit() {
+    this.valor = 5;
   }
 
+  mudarvalor(){
+    this.valor++;
+  }
+  reiniciarValor(){
+    this.valor = 0;
+  }
+
+  exibirTabela(){
+    this.exibeTabela = true;
+  }
 }
