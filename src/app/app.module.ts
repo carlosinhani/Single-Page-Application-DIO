79b725a60839eb56639c469e2604cb8d9780581c
Carlos.inhani <carlos.inhani@gmail.com>
2021-06-26 09:28:19 -0300

    alunos_professores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ProfessoresComponent } from './pages/professores/professores.component';
+import { AlunosComponent } from './pages/alunos/alunos.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,13 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AlunosComponent,
+    ProfessoresComponent
   ],
   imports: [
     BrowserModule,
